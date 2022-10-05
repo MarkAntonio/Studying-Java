2d7fa93f744a28344942174c94518436f596f20c
Marco Antonio <marcosdesouza772@gmail.com>
2022-10-05 15:10:47 -0300

    studying

--- aula01/src/aula01/ImprimindoVariaveis.java
+++ aula01/src/aula01/ImprimindoVariaveis.java
@@ -14,7 +14,22 @@ public class ImprimindoVariaveis {
 		}
 		else 
 			System.out.println("Tá novin em.");
+		int age = 30;
+		double salary_double = 3000;
+		float salary_float = 3000;
+		byte age_byte = 24;
+		short age_short = 32767;
+		boolean verdadeiro = true;
+		boolean falso = false;
+		long big_number = 3000L;
+		char caractere = 'A';  //2 bytes - permites kanjis japoneses, chineses et
+		// no char: ou eu coloco um numero inteiro (menor que 2 bytes) e ele vai traduzir para a tabela unicode, ou eu coloco o valor representando exadecimal
+		char caractere2 = '\u0042';
+		String name = "Marco";
+		System.out.println(caractere);
+		System.out.println(caractere2);
 		
+			
 		
 	}
 	/*
