345c6da4bbca0023d69506b701956637728c05bd
MarkAntonio <marcosdesouza772@gmail.com>
2022-10-17 16:16:17 -0300

    ex7

---
+++ aula01/src/dio/DioExer7.java
@@ -0,0 +1,9 @@
+package dio;
+
+public class DioExer7 {
+	public static void main(String[] args) {
+		int nota = 10;
+		String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+	}
+}
