b862954d4ab364efef43b058664110e5b132189d
MarkAntonio <marcosdesouza772@gmail.com>
2022-10-18 11:03:41 -0300

    ex13

---
+++ aula01/src/dio/DioExer13.java
@@ -0,0 +1,19 @@
+package dio;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class DioExer13 {
+	public static void main(String[] args) {
+		//Exemplo exceção
+		Number valor;
+		try {
+			valor = NumberFormat.getInstance().parse("a10.5");
+			System.out.println(valor);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
